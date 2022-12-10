01a561c1a3276125c81bdc8da06f8fbba29fe2c7
Najem Yohanna <najemyohanna@gmail.com>
2022-12-10 08:41:39 -0800

    puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-       	puts("\"Programming is like building a multilingua puzzel");
+       	puts("\"Programming is like building a multilingual puzzel");
        	return (0);
 }
