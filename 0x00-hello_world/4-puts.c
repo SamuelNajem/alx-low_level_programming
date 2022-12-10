21130e646bc15225a824f5fcc80bd35f7b553b56
Najem Yohanna <najemyohanna@gmail.com>
2022-12-10 07:58:07 -0800

    Cprogram

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - program that display wit put statement
+ *
+ * Return 0
+ *
+*/
+
+int main(void)
+{
+	puts("\nProgramming is like building a multilingua puzzel");
+	return(0);
+}
