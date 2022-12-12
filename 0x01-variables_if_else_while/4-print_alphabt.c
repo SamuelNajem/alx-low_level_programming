65b72b1d5a66c0bee003f2d6b0f257e9f15aad09
Najem Yohanna <najemyohanna@gmail.com>
2022-12-12 13:11:29 -0800

    lower case commit without e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main block code here
+ *
+ * Documentation: program to print out lower case with exception of q & e
+ *
+ * Retun: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
