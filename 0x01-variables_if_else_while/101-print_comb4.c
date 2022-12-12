4845487683b5e7268d2723eb6eec96a53d59a055
Najem Yohanna <najemyohanna@gmail.com>
2022-12-12 15:26:58 -0800

    task 11 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program for display of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k = 0;
+
+	while (k < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			i = 0;
+			while (i < 10)
+			{
+				if (i != j && j != k && k < j && j < i)
+				{
+					putchar('0' + k);
+					putchar('0' + j);
+					putchar('0' + i);
+					if (i + j + k != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				i++;
+			}
+			j++;
+		}
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
