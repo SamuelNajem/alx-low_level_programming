b2a076add3ce40c0691619d60b844ec2e0032081
Najem Yohanna <najemyohanna@gmail.com>
2022-12-12 16:11:05 -0800

    task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - program to display size of c data types
+ * main - Entry point
+ *
+ * Description: program to display size of data types in c
  *
  * Return: Always 0 (Success)
- *
-*/
+ */
 
 int main(void)
 {
@@ -14,11 +15,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	
-	printf("Size of char: %lu bytes.\n", (unsigned long)sizeof(a));
-	printf("Size of int: %lu bytes.\n", (unsigned long)sizeof(b));
-	printf("Size of long int: %lu bytes.\n", (unsigned long)sizeof(c));
-	printf("Size of long long int: %lu bytes.\n", (unsigned long)sizeof(d));
-	printf("Size of floa is %lu bytes.\n", (unsigned long)sizeof(e));
+
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of floa is %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
