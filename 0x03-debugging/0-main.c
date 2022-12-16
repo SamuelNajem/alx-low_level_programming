6e4ce6a8260283c5ad1db22ede263f7ca2b6368f
Najem Yohanna <najemyohanna@gmail.com>
2022-12-15 20:11:13 -0800

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include main.h
+
+/**
+ *  main - main block here
+ *
+ *  descript: debugging task
+ *
+ *  Return 0: Always 0 (succes)
+ */
+
+int main(void)
+{
+int i;
+
+i=0;
+positive_or_negative(i);
+
+return(0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void positive_or_negative(int i);
+#endif
